2b961ddf031f311c9d5dc4bbec9f74eef2f998a6
getlnmp <x@getlnmp.com>
2025-06-15 08:56:40 -0400

    update phpmyadmin version

--- include/version.sh
+++ include/version.sh
@@ -84,7 +84,7 @@ if [[ "${PHPSelect}" =~ ^[123]$ ]]; then
 elif [[ "${PHPSelect}" =~ ^[456]$ ]]; then
     PhpMyAdmin_Ver='phpMyAdmin-4.9.11-all-languages'
 else
-    PhpMyAdmin_Ver='phpMyAdmin-5.2.1-all-languages'
+    PhpMyAdmin_Ver='phpMyAdmin-5.2.2-all-languages'
 fi
 APR_Ver='apr-1.7.4'
 APR_Util_Ver='apr-util-1.6.3'
