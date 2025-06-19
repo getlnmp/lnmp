7c21b9e55f01fd8d14a84c6f328bb52a8483209c
getlnmp <x@getlnmp.com>
2025-06-19 11:20:30 -0400

    improve composer installation

--- include/php.sh
+++ include/php.sh
@@ -145,10 +145,10 @@ PHP_with_Imap()
             if echo "${CentOS_Version}" | grep -Eqi "^9" || echo "${Alma_Version}" | grep -Eqi "^9" || echo "${Rocky_Version}" | grep -Eqi "^9"; then
                 if ! rpm -qa | grep "libc-client-2007f" || ! rpm -qa | grep "uw-imap-devel"; then
                     if [ "${CheckMirror}" = "n" ]; then
-                        rpm -ivh ${cur_dir}/src/libc-client-2007f-24.el9.${ARCH}.rpm ${cur_dir}/src/uw-imap-devel-2007f-24.el9.${ARCH}.rpm
+                        rpm -ivh ${cur_dir}/src/libc-client-2007f-30.el9.${ARCH}.rpm ${cur_dir}/src/uw-imap-devel-2007f-30.el9.${ARCH}.rpm
                     else
-                        rpm -ivh ${Download_Mirror}/lib/uw-imap/libc-client-2007f-24.el9.${ARCH}.rpm
-                        rpm -ivh ${Download_Mirror}/lib/uw-imap/uw-imap-devel-2007f-24.el9.${ARCH}.rpm
+                        rpm -ivh ${libc-client-2007f-24_el9_DL}
+                        rpm -ivh ${uw-imap-devel-2007f-24_el9_DL}
                     fi
                 fi
             fi
@@ -211,19 +211,19 @@ Install_Composer()
     if [ "${CheckMirror}" != "n" ]; then
         echo "Downloading Composer..."
         if echo "${PHPSelect}" | grep -Eqi '^[1-8]' || echo "${php_version}" | grep -Eqi '^5.[2-6].*|7.[0-2].*' || echo "${Php_Ver}" | grep -Eqi "php-5.[2-6].*|php-7.[0-2].*"; then
-            wget --progress=dot:giga --prefer-family=IPv4 --no-check-certificate -T 120 -t3 ${Download_Mirror}/web/php/composer/composer-2.2.phar -O /usr/local/bin/composer
-            if [ $? -eq 0 ]; then
-                echo "Composer install successfully."
-                chmod +x /usr/local/bin/composer
-            else
-                echo "Composer install failed, try to from composer official website..."
-                curl -sS --connect-timeout 30 -m 60 https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --2.2
+#            wget --progress=dot:giga --prefer-family=IPv4 --no-check-certificate -T 120 -t3 ${Download_Mirror}/web/php/composer/composer-2.2.phar -O /usr/local/bin/composer
+#            if [ $? -eq 0 ]; then
+#                echo "Composer install successfully."
+#                chmod +x /usr/local/bin/composer
+#            else
+#                echo "Composer install failed, try to from composer official website..."
+                curl -sS --connect-timeout 30 -m 60 https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer --2.2.25
                 if [ $? -eq 0 ]; then
                     echo "Composer install successfully."
                 fi
             fi
         else
-            wget --progress=dot:giga --prefer-family=IPv4 --no-check-certificate -T 120 -t3 https://mirrors.aliyun.com/composer/composer.phar -O /usr/local/bin/composer
+            wget --progress=dot:giga --prefer-family=IPv4 --no-check-certificate -T 120 -t3 ${Download_Mirror}/web/php/composer/composer-2.2.25.phar -O /usr/local/bin/composer
             if [ $? -eq 0 ]; then
                 echo "Composer install successfully."
                 chmod +x /usr/local/bin/composer
